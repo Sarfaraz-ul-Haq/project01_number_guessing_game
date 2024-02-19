d54e2e2101a2b240cc5ecc8360ea2a787af3bdac
Sarfaraz <isarfaraz80@gmail.com>
2024-02-19 19:35:18 +0500

    remove imports from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 #! /usr/bin/env node
 
-import inquirer from "inquirer";
-import chalk from "chalk";
-import showBanner from "node-banner";
 import { displayGameTitleAndTagline } from "./gameTitle.js";
 import { promptUserToGuessNum } from "./guessTheNum.js";
 import { promptUserToPlayAgain } from "./playAgain.js";
